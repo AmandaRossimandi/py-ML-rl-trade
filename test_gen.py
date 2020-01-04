88b78319996be777323f481c7070a37a059524b3
claudiu <claudiu.schiller@gmail.com>
2020-01-04 23:44:56 +0200

    fixed test_gen file to match Close column name convention

--- test_gen.py
+++ test_gen.py
@@ -15,7 +15,7 @@ plt.plot(xsin)
 plt.show()
 
 dummy_cols = pd.DataFrame(np.ones((len(xsin), 4)))
-test_df = dummy_cols.join(pd.DataFrame(xsin, columns=['Val']))
+test_df = dummy_cols.join(pd.DataFrame(xsin, columns=['Close']))
 test_df.to_csv(output_path+"test_sinus.csv", index=False)
 
 xrise = [i for i in np.linspace(100, 200, PER*RPT, endpoint=False)]
@@ -24,5 +24,5 @@ plt.plot(xrise)
 plt.show()
 
 dummy_cols = pd.DataFrame(np.ones((len(xrise), 4)))
-test_df = dummy_cols.join(pd.DataFrame(xrise, columns=['Val']))
+test_df = dummy_cols.join(pd.DataFrame(xrise, columns=['Close']))
 test_df.to_csv(output_path+"test_rise_only.csv", index=False)
