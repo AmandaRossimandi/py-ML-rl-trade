5224b5d20a00bcfbfc852410491dfb2e0d804f6d
Samuel Cardonis <o528060707@gmail.com>
2020-01-05 13:36:22 +0200

    format

--- rl_dqn.py
+++ rl_dqn.py
@@ -50,13 +50,13 @@ if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--stock_name', '-s', type=str, default='test_sinus')  # ^GSPC_2001_2010  ^GSPC_1970_2018  ^GSPC_2011
-    parser.add_argument('--num_features', '-nf', type=int, default=20)  # (int) > 0
-    parser.add_argument('--num_neurons', '-nn', type=int, default=64)  # # (int) > 0
-    parser.add_argument('--episodes', '-e', type=int, default=20)  # (int) > 0 ,minimum 200 episodes for results. episode represent trade and learn on all data.
-    parser.add_argument('--batch_size', '-bs', type=int, default=20)  # (int) > 0 size of a batched sampled from replay buffer for training
-    parser.add_argument('--random_action_decay', type=float, default=0.99999)  # (float) 0-1
-    parser.add_argument('--future_reward_importance', type=float, default=0.05)  # (float) 0-1 aka decay or discount rate, determines the importance of future rewards
+    parser.add_argument('--stock_name'              , '-s' , type=str, default='test_sinus')  # ^GSPC_2001_2010  ^GSPC_1970_2018  ^GSPC_2011
+    parser.add_argument('--num_features'            , '-nf', type=int, default=20)  # (int) > 0
+    parser.add_argument('--num_neurons'             , '-nn', type=int, default=64)  # # (int) > 0
+    parser.add_argument('--episodes'                , '-e' , type=int, default=20)  # (int) > 0 ,minimum 200 episodes for results. episode represent trade and learn on all data.
+    parser.add_argument('--batch_size'              , '-bs', type=int, default=20)  # (int) > 0 size of a batched sampled from replay buffer for training
+    parser.add_argument('--random_action_decay'            , type=float, default=0.99999)  # (float) 0-1
+    parser.add_argument('--future_reward_importance'       , type=float, default=0.05)  # (float) 0-1 aka decay or discount rate, determines the importance of future rewards
     # If=0 agent will only learn to consider current rewards. if=1 it will make it strive for a long-term high reward.
 
     args = parser.parse_args()
